7ccd99a4d57a76c3eb3d7aff3f2cfca01aa8ce64
kinfei <kinfei1230@gmail.com>
2022-01-07 15:20:04 +0800

    Initial Hello World with show alert message

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -8,9 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
+        
+        Button("Show Alert") {
+            showingAlert = true
+        }
+        .alert("Important message", isPresented: $showingAlert) {
+            Button("Delete", role: .destructive) { }
+            Button("Cancel", role: .cancel) { }
+        } message: {
+            Text("Sure to delete?")
+        }
     }
 }
 
